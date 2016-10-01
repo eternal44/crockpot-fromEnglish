babc278e3e42f6c866f5b45b4f02df63d42f900c
James Youn <jamesyoun710@gmail.com>
2016-10-01 12:47:52 -0700

    Refactor and add tests

--- spec/mainSpecs.js
+++ spec/mainSpecs.js
@@ -1,10 +1,16 @@
 var tap = require('tap')
 var fromEnglish = require('../index.js')
 
-tap.equal(fromEnglish('four'), 4)
-tap.equal(fromEnglish('a hundred'), 100)
-tap.equal(fromEnglish('fourty two'), 42)
-tap.equal(fromEnglish('five hundred fifty-two'), 552)
-tap.equal(fromEnglish('fourteen'), 14)
-tap.equal(fromEnglish('two-hundred thousand eight-hundred and forty-two'), 200842)
-tap.equal(fromEnglish('two-hundred fifty-two thousand eight-hundred and forty-two'), 252842)
+var specList = [
+  {english: 'four', decimal: 4},
+  {english: 'a hundred', decimal: 100},
+  {english: 'fourty two', decimal: 42},
+  {english: 'five hundred fifty-two', decimal: 552},
+  {english: 'fourteen', decimal: 14},
+  {english: 'two-hundred thousand eight-hundred and forty-two', decimal: 200842},
+  {english: 'two-hundred fifty-two thousand eight-hundred and forty-two', decimal: 252842},
+]
+
+specList.forEach(function(param) {
+  tap.equal(fromEnglish(param.english), param.decimal)
+})
